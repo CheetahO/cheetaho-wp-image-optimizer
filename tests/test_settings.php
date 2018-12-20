ecb361ab643c13d8da548d362f3f060f5006b4a6
Nerijus <nerijus.oftas@gmail.com>
2018-12-20 20:36:01 +0200

    Create test_settings.php

---
+++ tests/test_settings.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class SettingsTest
+ *
+ * @package cheetaho-wp-image-optimizer
+ */
+/**
+ * Settings test case.
+ */
+class SettingsTest extends WP_UnitTestCase {
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
